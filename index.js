f71fbd2dfbce8555c9359a0ee4fb0125966451d9
salehaKhubaib <144957057+salehaKhubaib@users.noreply.github.com>
2023-11-25 23:22:14 +0500

    npm added

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+  
+  module.exports = reverseString; // Export the function to use in other files
+  
---
+++ node_modules/is-saleha/test-folder/script.js
@@ -0,0 +1,7 @@
+// app.js
+
+const reverseString = require('is-saleha'); // Assuming both files are in the same directory
+
+let originalString = "Saleha Khubaib";
+let reversed = reverseString(originalString);
+console.log(reversed); // Outputs: "!dlroW ,olleH"
